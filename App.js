7f88a7ee6e0c389cd04a74f504d8dfcbf97956fb
Mahmoud sharifi <113373073+mahmoudsharifi@users.noreply.github.com>
2023-06-23 12:17:12 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,60 @@
+import { Alert, StyleSheet } from "react-native";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  disableNetwork,
+  enableNetwork,
+} from "firebase/firestore";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
+import { useNetInfo } from "@react-native-community/netinfo";
+import { useEffect } from "react";
+import { getStorage } from "firebase/storage";
 
+const Stack = createNativeStackNavigator();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAg6V4s7l0uixp6kuypAvTTAZ7-2OpsLcQ",
+  authDomain: "chatapp-82cad.firebaseapp.com",
+  projectId: "chatapp-82cad",
+  storageBucket: "chatapp-82cad.appspot.com",
+  messagingSenderId: "222990206597",
+  appId: "1:222990206597:web:ba0b015a790058abfe4bec",
+  measurementId: "G-FMC9104NJF"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const storage = getStorage(app);
 
 const App = () => {
-  const Stack = createNativeStackNavigator();
+  const connectionStatus = useNetInfo();
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("Connection Lost!");
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
-        <Stack.Screen name='Start' component={Start} options={{ headerShown: false }} />
-        <Stack.Screen name='Chat' component={Chat}/>
+        <Stack.Screen name='Start' component={Start} />
+        <Stack.Screen name='Chat'>
+          {(props) => (
+            <Chat
+              isConnected={connectionStatus.isConnected}
+              db={db}
+              storage={storage}
+              {...props}
+            />
+          )}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
